4c3702570565b1f3d9faff404e798b0e607a0598
robynainsley21 <robynainsley2i@gmail.com>
2023-10-09 14:41:52 +0200

    Completed exercises 1.3-1.5

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,58 +1,55 @@
 import { useState } from "react";
 import React from "react";
 
-const Home = (props) => {
-  return <p>My name its {props.name}</p>;
-};
-
 const App = () => {
   const course = {
-    name: "Half Stack application development",
+    name: 'Half Stack application development',
     parts: [
       {
-        name: "Fundamentals of React",
+        name: 'Fundamentals of React',
         exercises: 10,
       },
       {
-        name: "Using props to pass data",
+        name: 'Using props to pass data',
         exercises: 7,
       },
       {
-        name: "State of a component",
+        name: 'State of a component',
         exercises: 14,
       },
-    ],
-  };
+    ]
+  }
 
   // components
   const Header = (props) => {
+    console.log(props);
     return <h1>{props.course} </h1>;
   };
 
   const Content = (props) => {
+    console.log(props)
     return (
       <>
-        <p>{props.part[0].name} {props.part[0].exercises}</p>
-        <p>{props.part[1].name} {props.part[1].exercises}</p>
-        <p>{props.part[2].name} {props.part[2].exercises}</p>
+        <p>{props.parts[0].name} | {props.parts[0].exercises}</p>
+        <p>{props.parts[1].name} | {props.parts[1].exercises}</p>
+        <p>{props.parts[2].name} | {props.parts[2].exercises}</p>
 
       </>
     );
   };
 
   const Total = (props) => {
+
     return (
-      <p>Number of exercises {props.part[0].exercises + props.part[1].exercises + props.part[2].exercises}</p>
+      <p>Number of exercises: {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
     )
   }
-
-
 
   return (
     <div>
       <Header course={course.name} />
-      <Content part={course.parts} />
-      <Total part={course.parts}/>
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
     </div>
   );
 };
